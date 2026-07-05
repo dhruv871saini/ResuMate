95fc2ac49a104a30c7f01b5ce0d5cee108dace7c
dhruv saini <dhruvsaini871@gmail.com>
2026-07-05 20:57:40 +0530

    add frontend deploy url for cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://resumate-git-main-dhruvs-projects-643f17f1.vercel.app/'], credentials: true }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'API Running' });
